7a447a25d25ddbcf5e30cb2faa21cc36e39e3ba7
FaisalSaeed-del <shoaibchudhary678@gmail.com>
2022-11-17 13:19:38 +0500

    data center section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import DataCenter from "./components/datacenter/DataCenter";
 import Navbar from "./components/nav/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <DataCenter />
     </div>
   );
 }
